8448c2924e1481c1fb8ad4e371ff0a789b6cb7d0
usama-binary-brix <99108743+usama216@users.noreply.github.com>
2025-11-24 13:10:44 +0500

    update base url

--- lib/api/baseApi.ts
+++ lib/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://api.dailybetterjourney.com/api',
     prepareHeaders: (headers, { getState }) => {
       headers.set('Content-Type', 'application/json')
       
